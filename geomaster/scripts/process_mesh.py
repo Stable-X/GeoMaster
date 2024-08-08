abb1444b474225139c2ec5c7c585a28b8bf3074a
hugoycj <576550489@qq.com>
2024-08-09 01:37:04 +0800

    feat: Add mesh cleaning

--- geomaster/scripts/process_mesh.py
+++ geomaster/scripts/process_mesh.py
@@ -15,18 +15,60 @@ from gaustudio.cameras.camera_paths import get_path_from_orbit
 import torchvision
 from torch.optim import Adam
 import numpy as np
+import logging
+logging.basicConfig(level=logging.INFO)
+import open3d as o3d
 
 @click.command()
 @click.option('--model_path', '-m', required=True, help='Path to the model')
 @click.option('--output_path', '-o', help='Path to the output mesh')
-@click.option('--sap_res', default=512, type=int, help='SAP resolution')
+@click.option('--sap_res', default=384, type=int, help='SAP resolution')
 @click.option('--num_sample', default=50000, type=int, help='Number of samples')
 @click.option('--sig', default=2, type=int, help='Sigma value')
 @click.option('--refine', default=False, type=bool, help='Whether to refine')
+@click.option('--clean', default=False, type=bool, help='Whether to clean')
 @click.option('--occ', default=False, type=bool, help='Whether to generate occ')
-def main(model_path: str, output_path: str, sap_res: int, num_sample: int, sig: int = 2, refine: bool =False, occ: bool = False) -> None:
+def main(model_path: str, output_path: str, sap_res: int, num_sample: int, sig: int = 2, refine: bool =False, clean: bool=False, occ: bool = False) -> None:
     if output_path is None:
         output_path = model_path[:-4]+'.refined.ply'
+    
+    if clean:
+        clean_path = model_path[:-4]+'.clean.ply'
+        logging.info("Cleaning mesh.")
+        ratio_threshold = 0.5
+        mesh = o3d.io.read_triangle_mesh(model_path)
+        
+        print("Analyzing connected components...")
+        (triangle_clusters, 
+        cluster_n_triangles,
+        cluster_area) = mesh.cluster_connected_triangles()
+        
+        print("Finding largest component...") 
+        triangle_clusters = np.array(triangle_clusters)  
+        cluster_n_triangles = np.array(cluster_n_triangles)
+        
+        largest_cluster_idx = cluster_n_triangles.argmax()
+        largest_cluster_n_triangles = cluster_n_triangles[largest_cluster_idx]
+
+        print(f"Largest component has {largest_cluster_n_triangles} triangles")
+        
+        triangles_keep_mask = np.zeros_like(triangle_clusters, dtype=np.int32)
+        saved_clusters = []
+        
+        print("Removing small components...")
+        for i, n_tri in enumerate(cluster_n_triangles):
+            if n_tri > ratio_threshold * largest_cluster_n_triangles:
+                saved_clusters.append(i)
+                triangles_keep_mask += triangle_clusters == i
+                
+        triangles_to_remove = triangles_keep_mask == 0
+        mesh.remove_triangles_by_mask(triangles_to_remove)
+        mesh.remove_unreferenced_vertices()
+        
+        print(f"Removed {triangles_to_remove.sum()} triangles")
+        o3d.io.write_triangle_mesh(clean_path, mesh)
+        model_path = clean_path
+        
     # Get original mesh
     gt_vertices, gt_faces = get_mesh(model_path)
     gt_vertices, gt_faces = gt_vertices.cuda(), gt_faces.cuda()
@@ -138,6 +180,7 @@ def main(model_path: str, output_path: str, sap_res: int, num_sample: int, sig: 
         mesh = Trimesh(vertices=vertices.detach().cpu().numpy(), faces=faces.detach().cpu().numpy(), 
                     vertex_normals=normals.detach().cpu().numpy())
         mesh.export(output_path)
+    
     if occ:
         vertices, faces, _, _, _ = sap_generate(dpsr, psr2mesh, inputs, 0, 0.5)
         mesh = Trimesh(vertices=vertices.detach().cpu().numpy(), faces=faces.detach().cpu().numpy()) # vertices in [-0.5, 0.5]
