3a2c124ac7c3113e22e38c246aa1130c0be3d701
hugoycj <576550489@qq.com>
2024-08-09 17:59:28 +0800

    feat: Update mask predictor

--- geomaster/scripts/prepare_data.py
+++ geomaster/scripts/prepare_data.py
@@ -5,17 +5,20 @@ from PIL import Image
 import os
 import shutil
 from tqdm import tqdm
-from rembg import remove
 from gaustudio import datasets, models
 from gaustudio.pipelines import initializers
 
 @click.command()
 @click.option('--source_path', '-s', required=True, help='Path to the dataset')
-@click.option('--images', '-i', default="images", help='Path to the images')
-def main(source_path: str, images: str) -> None:
+@click.option('--images', '-i', default="images", help='Path to the images dir')
+@click.option('--masks', '-m', default="mask", help='Path to the masks dir')
+def main(source_path: str, images: str, masks: str) -> None:
+    torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
+    mask_predictor = torch.hub.load("hugoycj/GenPercept-hub", "GenPercept_Segmentation", trust_repo=True)
     normal_predictor = torch.hub.load("Stable-X/StableNormal", "StableNormal_turbo", trust_repo=True)
+
     output_normal_dir = os.path.join(source_path, "normals")
-    output_mask_dir = os.path.join(source_path, "mask")
+    output_mask_dir = os.path.join(source_path, masks)
     os.makedirs(output_normal_dir, exist_ok=True)
     os.makedirs(output_mask_dir, exist_ok=True)
 
@@ -31,7 +34,8 @@ def main(source_path: str, images: str) -> None:
             
         output_mask_path = os.path.join(output_mask_dir, image_name + '.png')
         if not os.path.exists(output_mask_path):
-            mask = remove(input_image, only_mask=True)
+            mask = mask_predictor.infer_pil(input_image)
+            mask = Image.fromarray(mask)
             mask.save(output_mask_path)
 
     dataset = datasets.make({"name": "colmap", "source_path": source_path, "masks": 'mask', 
