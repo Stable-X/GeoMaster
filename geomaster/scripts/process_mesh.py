edefad361f41e8116c9db6c355468e9b26adc46a
hugoycj <576550489@qq.com>
2024-10-09 00:24:53 +0800

    fix: Clean process code

--- geomaster/scripts/process_mesh.py
+++ geomaster/scripts/process_mesh.py
@@ -11,7 +11,6 @@ from geomaster.utils.occupancy_utils import check_mesh_contains
 from trimesh import Trimesh
 import nvdiffrast.torch as dr
 import torch.nn.functional as F
-from gaustudio.cameras.camera_paths import get_path_from_orbit
 import torchvision
 from torch.optim import Adam
 import numpy as np
@@ -21,60 +20,63 @@ import open3d as o3d
 
 @click.command()
 @click.option('--model_path', '-m', required=True, help='Path to the model')
-@click.option('--output_path', '-o', help='Path to the output mesh')
+@click.option('--output_dir', '-o', help='Path to the output mesh')
 @click.option('--sap_res', default=384, type=int, help='SAP resolution')
 @click.option('--num_sample', default=50000, type=int, help='Number of samples')
 @click.option('--sig', default=2, type=int, help='Sigma value')
-@click.option('--refine', default=False, type=bool, help='Whether to refine')
-@click.option('--clean', default=False, type=bool, help='Whether to clean')
-@click.option('--occ', default=False, type=bool, help='Whether to generate occ')
-def main(model_path: str, output_path: str, sap_res: int, num_sample: int, sig: int = 2, refine: bool =False, clean: bool=False, occ: bool = False) -> None:
-    if output_path is None:
-        output_path = model_path[:-4]+'.refined.ply'
-    
-    if clean:
-        clean_path = model_path[:-4]+'.clean.ply'
-        logging.info("Cleaning mesh.")
-        ratio_threshold = 0.5
-        mesh = o3d.io.read_triangle_mesh(model_path)
-        
-        print("Analyzing connected components...")
-        (triangle_clusters, 
-        cluster_n_triangles,
-        cluster_area) = mesh.cluster_connected_triangles()
-        
-        print("Finding largest component...") 
-        triangle_clusters = np.array(triangle_clusters)  
-        cluster_n_triangles = np.array(cluster_n_triangles)
-        
-        largest_cluster_idx = cluster_n_triangles.argmax()
-        largest_cluster_n_triangles = cluster_n_triangles[largest_cluster_idx]
+def main(model_path: str, output_dir: str, sap_res: int, num_sample: int, sig: int = 2) -> None:
+    os.makedirs(output_dir, exist_ok=True)
 
-        print(f"Largest component has {largest_cluster_n_triangles} triangles")
-        
-        triangles_keep_mask = np.zeros_like(triangle_clusters, dtype=np.int32)
-        saved_clusters = []
-        
-        print("Removing small components...")
-        for i, n_tri in enumerate(cluster_n_triangles):
-            if n_tri > ratio_threshold * largest_cluster_n_triangles:
-                saved_clusters.append(i)
-                triangles_keep_mask += triangle_clusters == i
-                
-        triangles_to_remove = triangles_keep_mask == 0
-        mesh.remove_triangles_by_mask(triangles_to_remove)
-        mesh.remove_unreferenced_vertices()
-        
-        print(f"Removed {triangles_to_remove.sum()} triangles")
-        o3d.io.write_triangle_mesh(clean_path, mesh)
-        model_path = clean_path
-        
+    clean_path = model_path[:-4]+'.clean.ply'
+    logging.info("Cleaning mesh.")
+    ratio_threshold = 0.5
+    mesh = o3d.io.read_triangle_mesh(model_path)
+    
+    print("Analyzing connected components...")
+    (triangle_clusters, 
+    cluster_n_triangles,
+    cluster_area) = mesh.cluster_connected_triangles()
+    
+    print("Finding largest component...") 
+    triangle_clusters = np.array(triangle_clusters)  
+    cluster_n_triangles = np.array(cluster_n_triangles)
+    
+    largest_cluster_idx = cluster_n_triangles.argmax()
+    largest_cluster_n_triangles = cluster_n_triangles[largest_cluster_idx]
+
+    print(f"Largest component has {largest_cluster_n_triangles} triangles")
+    
+    triangles_keep_mask = np.zeros_like(triangle_clusters, dtype=np.int32)
+    saved_clusters = []
+    
+    print("Removing small components...")
+    for i, n_tri in enumerate(cluster_n_triangles):
+        if n_tri > ratio_threshold * largest_cluster_n_triangles:
+            saved_clusters.append(i)
+            triangles_keep_mask += triangle_clusters == i
+            
+    triangles_to_remove = triangles_keep_mask == 0
+    mesh.remove_triangles_by_mask(triangles_to_remove)
+    mesh.remove_unreferenced_vertices()
+    
+    print(f"Removed {triangles_to_remove.sum()} triangles")
+    o3d.io.write_triangle_mesh(clean_path, mesh)
+    model_path = clean_path
+    
+    
     # Get original mesh
     gt_vertices, gt_faces = get_mesh(model_path)
     gt_vertices, gt_faces = gt_vertices.cuda(), gt_faces.cuda()
     gt_vertsw = torch.cat([gt_vertices, torch.ones_like(gt_vertices[:,0:1])], axis=1).unsqueeze(0)
     gt_vertices = gt_vertices.unsqueeze(0)
     gt_normals = get_normals(gt_vertices[:,:,:3], gt_faces.long())
+    
+    rotation_matrix = torch.tensor([[1, 0, 0],
+                            [0, 1, 0],
+                            [0, 0, -1]]).to(gt_vertices.device, gt_vertices.dtype)
+    gt_vertices = torch.matmul(gt_vertices, rotation_matrix)
+    gt_normals = torch.matmul(gt_normals, rotation_matrix)
+    
     
     # Initialize SAP
     psr2mesh = PSR2Mesh.apply
@@ -87,113 +89,23 @@ def main(model_path: str, output_path: str, sap_res: int, num_sample: int, sig: 
     inputs.requires_grad_(True)
     inputs_optimizer = Adam([{'params': inputs, 'lr': 0.01}])
     
-    # Generate camera path
-    cameras = get_path_from_orbit(center.cpu().numpy(), scale.cpu().numpy() * 1.5, elevation=0)
-
     # Cache SAP output
-    vertices, faces, _, _, _ = sap_generate(dpsr, psr2mesh, inputs, center, scale)
-    vertsw = torch.cat([vertices, torch.ones_like(vertices[:,0:1])], axis=1).unsqueeze(0)
-    normals = get_normals(vertsw[:,:,:3], faces.long())
+    vertices, faces, _, _, _ = sap_generate(dpsr, psr2mesh, inputs, 0, 0.5)
+    vertsw = torch.cat([vertices, torch.ones_like(vertices[:,0:1])], axis=1).unsqueeze(0)    
+    normals = get_normals(vertsw[:,:,:3], faces.long())    
     mesh = Trimesh(vertices=vertices.detach().cpu().numpy(), faces=faces.detach().cpu().numpy(), 
-                   vertex_normals=normals.detach().cpu().numpy())
-    mesh.export(output_path)
+                   vertex_normals=-normals.detach().cpu().numpy()) # vertices in [-0.5, 0.5]
+    mesh.export(os.path.join(output_dir, 'poisson_mesh.ply'))
     
-    # Refine Poisson Mesh
-    if refine:
-        # Generate gt normal map
-        for _id, _camera in enumerate(cameras):
-            _camera = _camera.to("cuda")
-            _w2c = _camera.extrinsics.T
-            _proj = _camera.projection_matrix
-            _pose = _w2c.permute(1, 0).contiguous()
-            resolution = _camera.image_width, _camera.image_height
-            
-            rot_verts = torch.einsum('ijk,ikl->ijl', gt_vertsw, _w2c.unsqueeze(0))
-            proj_verts = torch.einsum('ijk,ikl->ijl', rot_verts, _proj.unsqueeze(0))
-            
-            int32_faces = gt_faces.to(torch.int32)
-            rast_out, _ = dr.rasterize(glctx, proj_verts, int32_faces, resolution=resolution)
-            
-            # render depth
-            feat = torch.cat([rot_verts[:,:,:3], torch.ones_like(gt_vertsw[:,:,:1]), gt_vertsw[:,:,:3]], dim=2)
-            feat, _ = dr.interpolate(feat, rast_out, int32_faces)
-            rast_verts = feat[:,:,:,:3].contiguous()
-            pred_mask = feat[:,:,:,3:4].contiguous()
-            rast_points = feat[:,:,:,4:7].contiguous()
-            pred_mask = dr.antialias(pred_mask, rast_out, proj_verts, int32_faces).squeeze(-1)
-            
-            # render normal
-            feat, _ = dr.interpolate(gt_normals, rast_out, int32_faces)
-            pred_normals = feat.contiguous()
-            pred_normals = dr.antialias(pred_normals, rast_out, proj_verts, int32_faces)
-            pred_normals = F.normalize(pred_normals,p=2,dim=3)
+    from pysdf import SDF
+    f = SDF(vertices.detach().cpu().numpy(), faces.detach().cpu().numpy())
+    sample_points = np.random.uniform(low=-0.55, high=0.55, size=(100000, 3))
+    sample_occ = f.contains(sample_points)
+    sample_occ = np.packbits(sample_occ.astype(bool))
+    np.savez(os.path.join(output_dir, 'points.npz'), points=sample_points, occupancies=sample_occ)
 
-            # Convert normal to view space        
-            _camera.normal = _camera.worldnormal2normal(pred_normals[0])
-            # _camera.normal = pred_normals[0]
-            torchvision.utils.save_image( (-1 * _camera.normal.permute(2, 0, 1) + 1) / 2, 
-                                         f"out/normal_{_id}.png")
-            _camera.mask = pred_mask.squeeze(1).squeeze(-1)
-            
-            
-        optim_epoch = 10
-        batch_size = 8
-        pbar = tqdm(range(optim_epoch))
-        num = len(cameras)
-        for i in pbar:
-            perm = torch.randperm(num).cuda()
-            for k in range(0, batch_size):
-                _camera = cameras[perm[k]].to("cuda")
-                _w2c = _camera.extrinsics.T
-                _proj = _camera.projection_matrix
-                _pose = _w2c.permute(1, 0).contiguous()
-                resolution = _camera.image_width, _camera.image_height
-                
-                vertices, faces, _, _, _ = sap_generate(dpsr, psr2mesh, inputs, center, scale)
-                vertsw = torch.cat([vertices, torch.ones_like(vertices[:,0:1])], axis=1).unsqueeze(0)
-                normals = get_normals(vertsw[:,:,:3], faces.long())
-
-                rot_verts = torch.einsum('ijk,ikl->ijl', vertsw, _w2c.unsqueeze(0))
-                proj_verts = torch.einsum('ijk,ikl->ijl', rot_verts, _proj.unsqueeze(0))
-                
-                int32_faces = faces.to(torch.int32)
-                rast_out, _ = dr.rasterize(glctx, proj_verts, int32_faces, resolution=resolution)
-                
-                # render normal
-                feat, _ = dr.interpolate(normals, rast_out, int32_faces)
-                pred_normals = feat.contiguous()
-                pred_normals = dr.antialias(pred_normals, rast_out, proj_verts, int32_faces)
-                pred_normals = F.normalize(pred_normals,p=2,dim=3)
-
-                
-                # Compute Normal Loss
-                gt_normal = _camera.normal2worldnormal().detach()
-                # gt_normal = _camera.normal
-                gt_mask = _camera.mask.detach()
-                gt_normal_mask = (gt_mask > 0) & (rast_out[0,:,:,3] > 0)
-                normal_error = (1 - (pred_normals * gt_normal).sum(dim=3))     
-                normal_loss =  100 * normal_error[gt_normal_mask].mean()
-                inputs_optimizer.zero_grad()
-                normal_loss.backward()
-                inputs_optimizer.step()
-
-        mesh = Trimesh(vertices=vertices.detach().cpu().numpy(), faces=faces.detach().cpu().numpy(), 
-                    vertex_normals=normals.detach().cpu().numpy())
-        mesh.export(output_path)
-    
-    if occ:
-        vertices, faces, _, _, _ = sap_generate(dpsr, psr2mesh, inputs, 0, 0.5)
-        mesh = Trimesh(vertices=vertices.detach().cpu().numpy(), faces=faces.detach().cpu().numpy()) # vertices in [-0.5, 0.5]
-        output_normalize_path = model_path[:-4]+'.normalized.ply'
-        mesh.export(output_normalize_path)
-        
-        sample_points = np.random.uniform(low=-0.55, high=0.55, size=(100000, 3))
-        sample_occ = check_mesh_contains(mesh, sample_points)[0]
-        sample_occ = np.packbits(sample_occ.astype(bool))
-        np.savez(os.path.join(os.path.dirname(model_path), 'points.npz'), points=sample_points, occupancies=sample_occ)
-
-        surface_points, _ = mesh.sample(100000, return_index=True)
-        np.savez(os.path.join(os.path.dirname(model_path), 'pointcloud.npz'), points=surface_points)
-
+    surface_points, surface_indices = mesh.sample(100000, return_index=True)
+    surface_normals = mesh.face_normals[surface_indices]
+    np.savez(os.path.join(output_dir, 'pointcloud.npz'), points=surface_points, normals=surface_normals)
 if __name__ == "__main__":
     main()
