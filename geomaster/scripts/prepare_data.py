d3ccdbd9d1f1a237568d8262be6223c56db5be60
lzt02 <1367652538@qq.com>
2025-01-24 15:31:35 +0800

    feat: add image super-resolution functionality

--- geomaster/scripts/prepare_data.py
+++ geomaster/scripts/prepare_data.py
@@ -8,14 +8,25 @@ from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from gaustudio import datasets, models
 from gaustudio.pipelines import initializers
+from RealESRGAN import RealESRGAN
+import numpy as np
 
-def process_image(image_path, output_normal_dir, output_mask_dir, normal_predictor, mask_predictor):
+
+def process_image(image_path, output_normal_dir, output_mask_dir, normal_predictor, mask_predictor, superresolution):
     image_name = os.path.splitext(os.path.basename(image_path))[0]
     input_image = Image.open(image_path)
     
     output_normal_path = os.path.join(output_normal_dir, f"{image_name}.png")
     if not os.path.exists(output_normal_path):
-        normal_image = normal_predictor(input_image)
+            
+        if superresolution:
+            sr_model = RealESRGAN("cuda:0", scale=4)
+            sr_model.load_weights('weights/RealESRGAN_x4.pth', download=True)
+            sr_input_image = sr_model.predict(np.array(input_image).astype(np.uint8))
+            
+        normal_image = normal_predictor(sr_input_image, data_type="object")
+        # Resize normal_image to a quarter of its original size
+        normal_image = normal_image.resize((normal_image.width // 4, normal_image.height // 4))
         normal_image.save(output_normal_path)
         
     output_mask_path = os.path.join(output_mask_dir, f"{image_name}.png")
@@ -29,11 +40,11 @@ def process_image(image_path, output_normal_dir, output_mask_dir, normal_predict
 @click.option('--images', '-i', default="images", help='Path to the images dir')
 @click.option('--masks', '-m', default="mask", help='Path to the masks dir')
 @click.option('--num_workers', '-w', default=4, help='Number of worker threads')
-def main(source_path: str, images: str, masks: str, num_workers: int) -> None:
+@click.option('--superresolution', '-sr', default=1, help='Selector for whether to perform super-resolution processing')
+def main(source_path: str, images: str, masks: str, num_workers: int, superresolution: int) -> None:
     torch.hub._validate_not_a_forked_repo = lambda a, b, c: True
     mask_predictor = torch.hub.load("aim-uofa/GenPercept", "GenPercept_Segmentation", trust_repo=True)
-    # normal_predictor = torch.hub.load("Stable-X/StableNormal", "StableNormal_turbo", trust_repo=True)
-    normal_predictor = None
+    normal_predictor = torch.hub.load("hugoycj/StableNormal", "StableNormal_turbo", trust_repo=True, yoso_version='yoso-normal-v1-8-1')
     
     output_normal_dir = os.path.join(source_path, "normals")
     output_mask_dir = os.path.join(source_path, masks)
@@ -65,7 +76,7 @@ def main(source_path: str, images: str, masks: str, num_workers: int) -> None:
     
     highlight_start = "\033[1;32m" 
     highlight_end = "\033[0m"
-    highlighted_command = f"{highlight_start}gm-recon -s {source_path} -m {visual_hull_path}{highlight_end}"
+    highlighted_command = f"{highlight_start}gm-refine -s {source_path} -m {visual_hull_path}{highlight_end}"
     print(f"Done. Run {highlighted_command} to get the final result.")
 
 if __name__ == "__main__":
