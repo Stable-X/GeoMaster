59f55f8b59276ed710e359895cb74dfc334f48c6
immortalqx <immortalqx@gmail.com>
2025-01-27 16:18:45 +0800

    feat[gm-prepare-data] add tsdf-fusion method for initialization

--- geomaster/scripts/prepare_data.py
+++ geomaster/scripts/prepare_data.py
@@ -8,18 +8,19 @@ from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from gaustudio import datasets, models
 from gaustudio.pipelines import initializers
+from geomaster.systems.tsdf_fusion_pipeline import tsdf_fusion
 
 def process_image(image_path, output_normal_dir, output_mask_dir, normal_predictor, mask_predictor):
     image_name = os.path.splitext(os.path.basename(image_path))[0]
     input_image = Image.open(image_path)
     
     output_normal_path = os.path.join(output_normal_dir, f"{image_name}.png")
-    if not os.path.exists(output_normal_path):
+    if not os.path.exists(output_normal_path) and normal_predictor is not None:
         normal_image = normal_predictor(input_image)
         normal_image.save(output_normal_path)
         
     output_mask_path = os.path.join(output_mask_dir, f"{image_name}.png")
-    if not os.path.exists(output_mask_path):
+    if not os.path.exists(output_mask_path) and mask_predictor is not None:
         mask = mask_predictor.infer_pil(input_image)
         mask = Image.fromarray(mask)
         mask.save(output_mask_path)
@@ -31,9 +32,10 @@ def process_image(image_path, output_normal_dir, output_mask_dir, normal_predict
 @click.option('--num_workers', '-w', default=4, help='Number of worker threads')
 def main(source_path: str, images: str, masks: str, num_workers: int) -> None:
     torch.hub._validate_not_a_forked_repo = lambda a, b, c: True
-    mask_predictor = torch.hub.load("aim-uofa/GenPercept", "GenPercept_Segmentation", trust_repo=True)
-    # normal_predictor = torch.hub.load("Stable-X/StableNormal", "StableNormal_turbo", trust_repo=True)
-    normal_predictor = None
+    # mask_predictor = torch.hub.load("aim-uofa/GenPercept", "GenPercept_Segmentation", trust_repo=True)
+    mask_predictor = None
+    normal_predictor = torch.hub.load("Stable-X/StableNormal", "StableNormal_turbo", trust_repo=True)
+    # normal_predictor = None
     
     output_normal_dir = os.path.join(source_path, "normals")
     output_mask_dir = os.path.join(source_path, masks)
@@ -54,19 +56,22 @@ def main(source_path: str, images: str, masks: str, num_workers: int) -> None:
         "data_device": "cuda", 
         "w_mask": True
     })
-    initializer = initializers.make({"name":"VisualHull", 
-                                     "radius_scale": 2.5,
-                                     "resolution": 256})
-    pcd = models.make("general_pcd")
-    initializer(pcd, dataset)
-    
-    visual_hull_path = os.path.join(source_path, 'visual_hull.ply')
-    shutil.copy(os.path.join(initializer.ws_dir, 'visual_hull.ply'), visual_hull_path)
-    
-    highlight_start = "\033[1;32m" 
-    highlight_end = "\033[0m"
-    highlighted_command = f"{highlight_start}gm-recon -s {source_path} -m {visual_hull_path}{highlight_end}"
-    print(f"Done. Run {highlighted_command} to get the final result.")
+
+    tsdf_fusion(dataset, source_path)
+
+    # initializer = initializers.make({"name":"VisualHull",
+    #                                  "radius_scale": 2.5,
+    #                                  "resolution": 256})
+    # pcd = models.make("general_pcd")
+    # initializer(pcd, dataset)
+    #
+    # visual_hull_path = os.path.join(source_path, 'visual_hull.ply')
+    # shutil.copy(os.path.join(initializer.ws_dir, 'visual_hull.ply'), visual_hull_path)
+    #
+    # highlight_start = "\033[1;32m"
+    # highlight_end = "\033[0m"
+    # highlighted_command = f"{highlight_start}gm-recon -s {source_path} -m {visual_hull_path}{highlight_end}"
+    # print(f"Done. Run {highlighted_command} to get the final result.")
 
 if __name__ == "__main__":
     main()
